050bcb8d5bc7be0715dedb22da5242433a361180
Felipe <felipetermometria@hotmail.com>
2024-02-15 15:06:37 -0300

    Room

--- app/src/main/java/com/example/app_planets/data/database/PlanetsDao.kt
+++ app/src/main/java/com/example/app_planets/data/database/PlanetsDao.kt
@@ -2,10 +2,14 @@ package com.example.app_planets.data.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface PlanetsDao {
 
     @Insert
     fun insert(planets : List<PlanetDatabaseEntity>)
+
+    @Query("SELECT * FROM planetdatabaseentity")
+     fun getAll(): List<PlanetDatabaseEntity>
 }
--- app/src/main/java/com/example/app_planets/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/app_planets/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -19,6 +20,13 @@ class MainFragment : Fragment() {
 
     private val viewModel: PlanetListViewModel by viewModels()
     private val adapter = PlanetsAdapter()
+
+
+    private val clickListener = { planet: PlanetData ->
+
+        Toast.makeText(requireContext(), "Planeta: ${planet.name}", Toast.LENGTH_SHORT).show()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
